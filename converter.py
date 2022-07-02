d18ff8ca412e63a92f3369379047ef3e69c1c5da
geogitlessons <geogitlessons@gmail.com>
2022-07-02 17:37:12 +0400

    Add kg to T option

--- converter.py
+++ converter.py
@@ -5,4 +5,12 @@
 unit = input("T or KG? ")
 amount = float(input("Amount: "))
 
-print(f"{amount * 1000} Kg.")
+if unit == "T":
+	print(f"{amount * 1000} Kg.")
+
+elif unit == "KG":
+	print(f"{amount / 1000} Kg.")
+
+else:
+	print("Error!")
+	
