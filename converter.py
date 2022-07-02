56dafd251ec43d9baff368c67daccd4e81a24582
geogitlessons <107221124+geogitlessons@users.noreply.github.com>
2022-07-02 17:52:09 +0400

    Add new features (#1)
    * Add unit choose option
    * Add kg to T option
    * Add README.txt

--- converter.py
+++ converter.py
@@ -1,5 +1,16 @@
 
 
-amount = float(input("Tonn Amount: "))
 
-print(f"{amount * 1000} Kg.")
+
+unit = input("T or KG? ")
+amount = float(input("Amount: "))
+
+if unit == "T":
+	print(f"{amount * 1000} Kg.")
+
+elif unit == "KG":
+	print(f"{amount / 1000} Kg.")
+
+else:
+	print("Error!")
+	
