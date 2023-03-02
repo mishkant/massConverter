88864db4bbcae748ad1b6072fdf92935f1749872
selenophiles2 <anna.patchkria@gmail.com>
2023-03-02 13:28:02 +0400

    add unit choose option

--- converter.py
+++ converter.py
@@ -1,16 +1,7 @@
 
 
-
-
 unit = input("T or KG? ")
 amount = float(input("Amount: "))
 
-if unit == "T":
-	print(f"{amount * 1000} Kg.")
 
-elif unit == "KG":
-	print(f"{amount / 1000} Kg.")
-
-else:
-	print("Error!")
 	
