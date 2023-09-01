3ac49a0c2819bdd852cd2743049e603a54171057
codemaster <levandemetrashvili10@gmail.com>
2023-09-01 23:08:41 +0400

    add Kg to T opt

--- converter.py
+++ converter.py
@@ -4,11 +4,21 @@
 
 
 	unit = input ("T or KG")
-	
+
 amount = float(input("Amount: "))
+
+if unit =="T":
+
 
 
 	print(f"{amount * 1000} Kg.")
+
+	elif unit = "KG":
+	print(f"{amount / 1000} Kg.")
+
+	else: 
+		print ("Error!")
+
 
 
 
